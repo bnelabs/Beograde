0b060151b75ffef5174da2d3639bb33ee5c361d7
Rahat <rahatimrahat@gmail.com>
2026-05-14 09:49:54 +0300

    feat(i18n): StringsService returns tr bundle when locale is tr

--- src/app/core/i18n/strings.service.ts
+++ src/app/core/i18n/strings.service.ts
@@ -3,6 +3,7 @@ import { I18nService } from './i18n.service';
 import enStrings from '../../../i18n/en.json';
 import srLatStrings from '../../../i18n/sr-Latn.json';
 import srCyrStrings from '../../../i18n/sr-Cyrl.json';
+import trStrings from '../../../i18n/tr.json';
 
 type StringsBundle = typeof enStrings;
 
@@ -11,6 +12,7 @@ export class StringsService {
   private readonly i18n = inject(I18nService);
   readonly t = computed<StringsBundle>(() => {
     const loc = this.i18n.locale();
+    if (loc.locale === 'tr') return trStrings as StringsBundle;
     if (loc.locale !== 'sr') return enStrings;
     if (loc.script === 'Cyrl' && this.i18n.cyrillicEnabled()) return srCyrStrings as StringsBundle;
     return srLatStrings;
